000e31003e1e445e24047452ba894b65c2894944
Amar Zavery <amzavery@microsoft.com>
2018-03-26 18:01:55 -0700

    Add validation to the connection config.

--- lib/connectionConfig.ts
+++ lib/connectionConfig.ts
@@ -37,6 +37,9 @@ export namespace ConnectionConfig {
    * @param {string} [path]           - The name/path of the entity (hub name) to which the connection needs to happen
    */
   export function create(connectionString: string, path?: string): ConnectionConfig {
+    if (!connectionString || (connectionString && typeof connectionString !== "string")) {
+      throw new Error("'connectionString' is a required parameter and must be of type: 'string'.");
+    }
     const parsedCS = parseConnectionString(connectionString);
     if (!path && !parsedCS.EntityPath) {
       throw new Error(`Either provide "path" or the "connectionString": "${connectionString}", must contain EntityPath="<path-to-the-entity>".`);
@@ -44,11 +47,33 @@ export namespace ConnectionConfig {
     const result: ConnectionConfig = {
       connectionString: connectionString,
       endpoint: parsedCS.Endpoint,
-      host: (parsedCS.Endpoint.match('sb://([^/]*)') || [])[1],
+      host: (parsedCS && parsedCS.Endpoint) ? (parsedCS.Endpoint.match('sb://([^/]*)') || [])[1] : "",
       entityPath: path || parsedCS.EntityPath,
       sharedAccessKeyName: parsedCS.SharedAccessKeyName,
       sharedAccessKey: parsedCS.SharedAccessKey
     };
     return result;
+  }
+
+  /**
+   * Validates the properties of connection config.
+   * @param {ConnectionConfig} config The connection config to be validated.
+   */
+  export function validate(config: ConnectionConfig): void {
+    if (!config || (config && typeof config !== "object")) {
+      throw new Error("'config' is a required parameter and must be of type: 'object'.");
+    }
+    if (!config.endpoint || (config.endpoint && typeof config.endpoint !== "string")) {
+      throw new Error("'endpoint' is a required property of the ConnectionConfig.");
+    }
+    if (!config.entityPath || (config.entityPath && typeof config.entityPath !== "string")) {
+      throw new Error("'entityPath' is a required property of the ConnectionConfig.");
+    }
+    if (!config.sharedAccessKeyName || (config.sharedAccessKeyName && typeof config.sharedAccessKeyName !== "string")) {
+      throw new Error("'sharedAccessKeyName' is a required property of the ConnectionConfig.");
+    }
+    if (!config.sharedAccessKey || (config.sharedAccessKey && typeof config.sharedAccessKey !== "string")) {
+      throw new Error("'sharedAccessKey' is a required property of the ConnectionConfig.");
+    }
   }
 }
