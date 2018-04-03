f53163921751a1377df54bac74aafc6b1430cb4c
Amar Zavery <amzavery@microsoft.com>
2018-04-02 21:02:07 -0700

    using ts-node to run mocha tests. This helps us avoid generating .js files for tests and type definitions in the typings folder.

--- typings/cli/commands/send.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="yargs" />
-import { Argv } from "yargs";
-export declare const command = "send";
-export declare const describe = "Sends messages to an eventhub.";
-export declare function handler(argv: Argv): void;
--- typings/testhub/commands/send.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="yargs" />
-import { CommandBuilder } from "yargs";
-export declare const command = "send";
-export declare const describe = "Sends messages to an eventhub.";
-export declare const builder: CommandBuilder;
-export declare function handler(argv: any): Promise<void>;
